8e5bf448feaae81f305e2ca490e942fd65979002
ThomasTheFaux <42686294+ThomasTheFaux@users.noreply.github.com>
2020-03-12 02:44:05 -0400

    Player movement and camera
    - added PlayerController script for player movement
    - added PlayerCameraLook script for first person camera looking
    - added Player prefab

---
+++ Assets/Scripts/PlayerCameraLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    //Purpose... 
+    //  + Player Camera Looking
+    //  + Taking in mouse as input
+    //
+    //Script By Thomas Joyce
+
+public class PlayerCameraLook : MonoBehaviour {
+	
+	float mouseSens = .7f;																				//mouse sensitivity
+	float mouseSmoo = 2f;																				//mouse smoothing
+	float minY = -70;																					//min angle for camera to look down
+	float maxY = 80;																					//max angle for camera to look up
+	Vector2 mouseLook;																					//vector for looking with the mouse
+	Vector2 smoothVect;																					//vector that smooths mouse movements
+	GameObject player;																					//player gameobject
+
+	void Start(){
+		Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+		player = this.transform.parent.gameObject;														//assigns player to the parent gameobject				
+	}
+	
+	void Update(){
+		playerLook();
+	}
+
+	void playerLook(){
+		var mouseDir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));			//records inputs from cursor
+
+		mouseDir = Vector2.Scale(mouseDir, new Vector2(mouseSens * mouseSmoo, mouseSens * mouseSmoo));	//calculates direction of mouse
+		smoothVect.x = Mathf.Lerp(smoothVect.x, mouseDir.x, .75f/ mouseSmoo);							//smooths x axis of smoothVect (added slightly slow multiplier)
+		smoothVect.y = Mathf.Lerp(smoothVect.y, mouseDir.y, 1f/ mouseSmoo);								//smooths y axis of smoothVect
+		mouseLook += smoothVect;																		//sets the mouse looking values
+		mouseLook.y = Mathf.Clamp (mouseLook.y, minY, maxY); 
+
+		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);					//rotates camera vertically			
+		player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);		//rotates player horizonally
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+    //Purpose... 
+    //  + Player Movement
+    //  - Upgrade Towers
+    //  - Repair Towers
+    //
+    //Script By Thomas Joyce
+
+public class PlayerController : MonoBehaviour {
+    
+    //Movement variables
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDist = 0.4f;
+    public float gravity = -9.81f;      
+    public float speed = 6f;                                    //current speed of player
+    float speedWalk = 6f;                                       //normal speed of player
+    float speedSpr = 0f;                                        //sprint speed of player
+    float jumpHeight = 1f;                                      //height of jump
+    Vector3 velocity;
+    bool isGrounded;                                            //if true, you are on the ground
+    
+
+
+    void Start(){
+        controller = GetComponent<CharacterController>();
+        speedWalk = speed;
+        speedSpr = speed * 1.5f;
+    }
+    
+    void Update(){
+        movement();
+        jumping();
+        sprinting();
+    }
+
+    //handles character movement
+    void movement(){
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+        
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    //handles jumping
+    void jumping(){
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+        if(isGrounded && Input.GetKeyDown("space")){
+            float jumpVel = Mathf.Sqrt (-2 * gravity * jumpHeight);
+            velocity.y = jumpVel;
+        }
+    }
+
+    //handles sprinting
+    void sprinting(){
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+        if(isGrounded && Input.GetKeyDown(KeyCode.LeftShift)){
+            speed = speedSpr;
+        }
+
+        if(isGrounded && Input.GetKeyUp(KeyCode.LeftShift)){
+            speed = speedWalk;
+        }
+
+        //checks if player is on ground, if not resets speed
+        if(!isGrounded){
+            speed = 9f;
+        }
+    }
+
+    //repairs turret
+    void repairTurret(){
+
+    }
+
+    //opens upgrade menu
+    void upgradeTurret(){
+
+    }
+}
